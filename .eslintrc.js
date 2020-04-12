fc6883252817b00d811e83edf99b8c5dfcc2c851
Connor Bär <connor.baer@me.com>
2020-04-12 15:27:50 +0200

    fix(configs): ignore /template when linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,12 @@ module.exports = require('@sumup/foundry/eslint')(
   {
     language: 'TypeScript',
     environments: ['Node'],
-    frameworks: ['React', 'Emotion', 'Jest'],
+    frameworks: ['Jest'],
     openSource: true,
   },
   {
     rules: {
       'no-process-exit': 'off',
     },
-    parserOptions: {
-      project: ['./tsconfig.eslint.json'],
-    },
-    overrides: [
-      {
-        files: ['template/**', '*.config.js'],
-        rules: {
-          'notice/notice': 'off',
-          'import/no-unresolved': 'off',
-          'node/no-missing-require': 'off',
-          'node/no-unpublished-require': 'off',
-          '@typescript-eslint/no-var-requires': 'off',
-        },
-      },
-    ],
   },
 );
