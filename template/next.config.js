6bcebd949f0d753a959ea14c8c81a65472024c3c
Connor Bär <connor-baer@users.noreply.github.com>
2020-05-06 14:20:06 +0200

    feat: rewrite template in TypeScript (#13)
    * feat: rewrite template in TypeScript
    
    * feat: update template dependencies

---
+++ template/next.config.js
@@ -0,0 +1,18 @@
+const withTM = require('next-transpile-modules')([
+  '@sumup/circuit-ui',
+  '@sumup/icons',
+]);
+
+module.exports = withTM({
+  /**
+   * Environment variables can be used to inject dynamic configuration
+   * into the Next.js app at built time. Learn more at
+   * https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+   */
+  env: {
+    SITE_NAME: 'SumUp',
+    SITE_LOCALE: 'en',
+    SITE_BASEURL: 'https://example.sumup.com',
+    STATIC_ASSET_BASEURL: 'https://static.sumup.com',
+  },
+});
---
+++ template/utils/styled.ts
@@ -0,0 +1,5 @@
+import styled, { CreateStyled } from '@emotion/styled';
+
+import { Theme } from '../types/theme';
+
+export default styled as CreateStyled<Theme>;
