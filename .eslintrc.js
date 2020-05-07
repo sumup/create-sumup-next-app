6167eef3140d66054614065812cf185895375fee
Connor Bär <connor.baer@me.com>
2020-05-07 11:26:40 +0200

    chore: fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,6 @@
-module.exports = require('@sumup/foundry/eslint')(
-  {
-    language: 'TypeScript',
-    environments: ['Browser'],
-    frameworks: ['Jest', 'Emotion', 'React'],
-    openSource: false,
-  },
-  {
-    overrides: [
-      {
-        files: ['template/**/*'],
-        rules: {
-          'import/no-unresolved': 'off',
-        },
-      },
-    ],
-  },
-);
+module.exports = require('@sumup/foundry/eslint')({
+  language: 'TypeScript',
+  environments: ['Browser'],
+  frameworks: ['Jest', 'Emotion', 'React'],
+  openSource: false,
+});
