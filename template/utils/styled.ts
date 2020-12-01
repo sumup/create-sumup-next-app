d0f29d57141915ef6ac4c966fdba330758d20e0b
Connor Bär <connor.baer@me.com>
2020-12-02 00:49:33 +0100

    refactor: import Theme type from design tokens

--- template/utils/styled.ts
+++ template/utils/styled.ts
@@ -1,6 +1,5 @@
 import styled, { CreateStyled } from '@emotion/styled';
-
-import { Theme } from '../types/theme';
+import { Theme } from '@sumup/design-tokens';
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 export default styled as CreateStyled<Theme>;
