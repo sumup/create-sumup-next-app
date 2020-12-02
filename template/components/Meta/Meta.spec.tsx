23c174b9b5ea1214648417b94f5cd89b0890c989
Connor Bär <connor.baer@me.com>
2020-12-02 01:35:55 +0100

    fix: ignore lint error

--- template/components/Meta/Meta.spec.tsx
+++ template/components/Meta/Meta.spec.tsx
@@ -8,7 +8,8 @@ import { Meta, MetaProps } from './Meta';
  * The Head component from Next.js renders its children into the head element
  * as a side-effect, meaning we can't access them unless we mock the component.
  */
-jest.mock('next/head', () => (props) => <div {...props} />);
+// eslint-disable-next-line react/display-name
+jest.mock('next/head', () => (props: any) => <div {...props} />);
 
 describe('Meta', () => {
   /**
